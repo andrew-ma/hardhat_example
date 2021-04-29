b1e48bf9a1402de52f7e9fac2d38602d767f570d
andrew-ma <andrew-ma@users.noreply.github.com>
2021-04-29 02:28:03 -0500

    Add output task for generating output file that works with remix

--- hardhat.config.js
+++ hardhat.config.js
@@ -1,4 +1,8 @@
 require("@nomiclabs/hardhat-waffle");
+const path = require("path");
+const exec = require("child_process").exec;
+
+const appRoot = path.resolve(__dirname);
 
 // This is a sample Hardhat task. To learn how to create your own go to
 // https://hardhat.org/guides/create-task.html
@@ -13,6 +17,24 @@ task("accounts", "Prints the list of accounts", async () => {
 task("alchemy_url", "Prints alchemy url", async () => {
     console.log(process.env.ALCHEMY_MAINNET_RPC_URL);
 });
+
+task("output", "Run flatten task with result passed through bash script and saved as file 'output.txt'")
+    .addParam("file", "The solidity file that will be flattened")
+    .setAction(async (taskArguments) => {
+        const bashScriptFilename = path.join(appRoot, "output_task.sh");
+
+        console.log(`Running \`bash "${bashScriptFilename}" "${taskArguments["file"]}"\``);
+
+        const myShellScript = exec(`bash "${bashScriptFilename}" "${taskArguments["file"]}"`);
+        console.log(`Generated file "${path.resolve(path.join(".", "output"))}"`);
+
+        myShellScript.stdout.on("data", (data) => {
+            console.log(data);
+        });
+        myShellScript.stderr.on("data", (data) => {
+            console.error(data);
+        });
+    });
 
 // You need to export an object to set up your config
 // Go to https://hardhat.org/config/ to learn more
---
+++ output_task.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+hh flatten $1 2>/dev/null > output
+
+# remove all SPDX comments
+sed -i 's/[/][/] SPDX-License-Identifier:.*$//' output
+
+# add SPDX comment to first line
+sed -i '1 i\// SPDX-License-Identifier: UNLICENSED' output
+
+# replace now with block.timestamp
+# sed -i 's/now/block.timestamp/g' output
